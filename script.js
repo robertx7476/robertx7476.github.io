72f92373df63e553128ba3d6cb221d5ba297d3c7
robertx7476 <146866561+robertx7476@users.noreply.github.com>
2023-10-03 12:50:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Robert Xu"; // update the part BETWEEN the "quotes"
+let userName = "robertx7476"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
